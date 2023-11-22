402207c942c8d5db7a01ce2c3bf03f7686ef3a74
sumeet <maheshwarisumeet128@gmail.com>
2023-11-22 11:07:37 +0530

    app.py file Updated.

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
-# import the important libraries
-from flask import Flask, Request, render_template, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, url_for
 from ocr_engine import OCREngine
-from utils.helper import allowed_filename, save_uploaded_file
-import os, sys
+from utils.helper import allowed_file, save_uploaded_file
+import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
-# ensure that uploads folder exists in directory
+# Ensure the 'uploads' directory exists
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.mkdir(app.config['UPLOAD_FOLDER'])
-
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
-        if file and allowed_filename(file.filename):
+        if file and allowed_file(file.filename):
             filename = save_uploaded_file(file)
             if filename:
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 ocr = OCREngine()
-                extract_text = ocr.extract_text(file_path=file_path)
-                return render_template("index.html", extract_text = extract_text)
-            return redirect(url_for('index'))
-        return render_template("index.html", extract_text = None)
-
+                extracted_text = ocr.extract_text(file_path)
+                return render_template('index.html', extracted_text=extracted_text)
+        return redirect(url_for('index'))
+    return render_template('index.html', extracted_text=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
