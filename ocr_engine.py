1247d68bee4111bf062e295523dbfd8db154911c
sumeet <maheshwarisumeet128@gmail.com>
2023-11-22 11:08:26 +0530

    ocr_enginer file created.

--- ocr_engine.py
+++ ocr_engine.py
@@ -1,4 +1,3 @@
-# importing the important libraries
 import pytesseract
 from PIL import Image
 import os
@@ -6,15 +5,9 @@ import os
 class OCREngine:
     @staticmethod
     def extract_text(file_path):
-        """
-        1. Open the image file path using Image library
-        2. Using Pytesseract library to extract text from the image
-        3. return the text
-        """
         try:
             image = Image.open(file_path)
             text = pytesseract.image_to_string(image)
             return text
         except Exception as e:
             return str(e)
-        
